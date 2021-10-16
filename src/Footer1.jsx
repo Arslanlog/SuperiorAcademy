bd47d17ea0c39e3822b0c5b167b043ca623fbc59
MHammad-100 <hammadmansha100@gmail.com>
2021-10-16 15:29:12 +0500

    footer map

--- src/Footer1.jsx
+++ src/Footer1.jsx
@@ -20,11 +20,11 @@ const icons = {
         width: "50px",
         textDdecoration: "none",
         margin: "5px 2px",
-        borderRadius:"10%",
-        
+        borderRadius: "10%",
 
-      },
-      twiter: {
+
+    },
+    twiter: {
         background: "#55ACEE",
         color: "white",
         padding: "20px",
@@ -32,10 +32,10 @@ const icons = {
         width: "50px",
         textDdecoration: "none",
         margin: "5px 2px",
-        borderRadius:"10%",
-        
-      },
-      whatsapp: {
+        borderRadius: "10%",
+
+    },
+    whatsapp: {
         background: "#green",
         color: "white",
         padding: "20px",
@@ -43,9 +43,9 @@ const icons = {
         width: "50px",
         textDdecoration: "none",
         margin: "5px 2px",
-        borderRadius:"10%",
-        
-      }
+        borderRadius: "10%",
+
+    }
 
 }
 
@@ -63,23 +63,33 @@ const Footer1 = () => {
                 <div>
                     <div style={footerLabel}> Follow US </div>
                     <div>
-                        <a style={icons.facebook} href="#" class="fa fa-facebook"></a>
+                        <a style={icons.facebook} href="https://web.facebook.com/superior.kasur.3" class="fa fa-facebook"></a>
                         <a style={icons.twiter} href="#" class="fa fa-twitter"></a>
                         <a style={icons.whatsapp} href="#" class="fa fa-whatsapp"></a>
                     </div>
                 </div>
                 <div>
                     <div style={footerLabel}> Our Location </div>
-                    <div></div>
-                </div>
-                <div>
-                    <div style={footerLabel}> Find US </div>
-                    <div></div>
-                </div>
+                    <div>
+                        <h2><strong>Our Location </strong></h2>
+                        <hr />
+                        <div>
+                            <h4>234/80 -UFG , New Street,</h4>
+                            <h4>Switzerland.</h4>
+                            <h4><strong>Call:</strong>  + 67-098-907-1269 / 70 / 71 </h4>
+                            <h4><strong>Email: </strong>info@yourdomain.com</h4>
+                        </div>
+                    </div>
+                    <div>
+                        <div style={footerLabel}> Find US </div>
+                        <div>
+                            <iframe src="https://maps.google.com/maps?width=600&height=400&hl=en&q=Superior Academy, Bips School, Azhar Colony Basti Khadimabad, Kasur&t=&z=14&ie=UTF8&iwloc=B&output=embed" width="100%" height={360} frameBorder={0} style={{ border: 0 }} allowFullScreen />
+                        </div>
+                    </div>
 
-            </div>
-        </>
-    )
+                </div>
+            </>
+            )
 }
 
-export default Footer1
+            export default Footer1
